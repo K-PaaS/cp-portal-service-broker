213fc6be5e11ca7059866ca6c52b88677ec60bbf
hoon77 <slokang@naver.com>
2021-12-30 16:43:12 +0900

    Upload container platform portal service broker source

--- README.md
+++ README.md
@@ -1,1 +1,101 @@
 # container-platform-portal-service-broker
+
+PaaS-TA에서 제공하는 Container Platform 포털 서비스 브로커로 클라우드 컨트롤러와 서비스 브로커간의 v2 서비스 API 를 제공합니다.
+
+- [시작하기](#시작하기)
+  - [Container Platform 포털 서비스 브로커 설치 방법](#Container-Platform-포털-서비스-브로커-설치-방법)
+  - [Container Platform 포털 서비스 브로커 빌드 방법](#Container-Platform-포털-서비스-브로커-빌드-방법)
+- [개발 환경](#개발-환경)
+- [가능한 명령 목록 (로컬 환경)](#가능한-명령-목록-(로컬-환경))
+- [라이선스](#라이선스)
+
+## 시작하기
+
+Container Platform 포털 서비스 브로커가 수행하는 서비스 관리 작업은 다음과 같습니다.
+- Catalog : Container 서비스 카탈로그 조회
+- Provisioning : Container 서비스 인스턴스 생성 ( parameters "owner", "org_name", "space_name" 필수 )
+- Updateprovisioning : Container 서비스 인스턴스 갱신
+- Deprovisioning : Container 서비스 인스턴스 삭제
+
+### Container Platform 포털 서비스 브로커 설치 방법
+
+[서비스팩 설치 가이드](https://github.com/PaaS-TA/Guide-5.0-Ravioli/blob/master/service-guide/tools/PAAS-TA_CONTAINER_SERVICE_INSTALL_GUIDE_V2.0.md)의 가이드를 참고하시면 아키텍쳐와 설치 및 사용법에 대해 자세히 알 수 있습니다.
+
+### Container Platform 포털 서비스 브로커 빌드 방법
+
+Container 서비스 브로커를 활용하여 로컬 환경에서 빌드하고 싶을 때 다음 명령어를 입력합니다.
+```
+$ gradle build
+```
+
+## 개발 환경
+
+Container 서비스 브로커의 개발 환경은 다음과 같습니다.
+
+| Situation                      | Version |
+| ------------------------------ | ------- |
+| JDK                            | 8       |
+| Gradle                         | 6.5     |
+| Spring Boot                    | 2.3.3   |
+| Spring Boot Cf Service Broker  | 2.4.0   |
+| JSch                           | 0.1.54  |
+| Hibernate Validator            | 5.1.0   |
+| Json Path                      | 2.2.0   |
+| Jacoco                         | 0.8.1   |
+| MariaDB Java Client            | 2.2.6   |
+
+
+## 가능한 명령 목록 (로컬 환경)
+
+- Catalog 조회 : http://localhost:8888/v2/catalog
+  - Method : GET 
+  - Header
+    > Authorization : Bearer type \
+      X-Broker-Api-Version : 2.4 \
+      Content-Type : application/json
+  - Body : None 
+  - Parameters : None
+
+- 서비스 인스턴스 생성 : http://localhost:8888/v2/service_instances/[new-instance-name]
+  - Method : PUT 
+  - Header
+    > Authorization : Bearer type \
+      X-Broker-Api-Version : 2.4 \
+      Content-Type : application/json \
+      Accept: application/json
+  - Body
+    > { \
+        "service_id": \<service-id-string\>, \
+        "plan_id": \<plan-id-string\>, \
+        "organization_guid": \<organization-guid-string\>, \
+        "space_guid": \<space-guid-string\>, \
+        "parameters": { "userName": \<user-name-in-caas-service\> } \
+      }
+  - Parameters : None
+
+- 서비스 인스턴스 갱신 : http://localhost:8888/v2/service_instances/[instance-name]
+  - Method : PATCH 
+  - Header
+    > Authorization : Bearer type \
+      X-Broker-Api-Version : 2.4 \
+      Content-Type : application/json 
+  - Body
+    > { \
+        "plan_id": \<plan-id-string\>, \
+        "service_id": \<service-id-string\> \
+      } 
+  - Parameters : None
+
+- 서비스 인스턴스 삭제 : http://localhost:8888/v2/service_instances/[instance-name]
+  - Method : DELETE 
+  - Header
+    > Authorization : Bearer type \
+      X-Broker-Api-Version : 2.4 \
+      Content-Type : application/json 
+  - Body : None
+  - Parameters : 
+    > service_id : \<predefined_service_id\> \
+      plan_id : \<plan_id_of_service\>
+
+## 라이선스
+Container 서비스 브로커는 [Apache-2.0 License](http://www.apache.org/licenses/LICENSE-2.0)를 사용합니다.
---
+++ src/main/java/org/openpaas/servicebroker/container/platform/service/impl/KeycloakAdminClientService.java
@@ -0,0 +1,219 @@
+package org.openpaas.servicebroker.container.platform.service.impl;
+
+
+
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+
+import org.keycloak.representations.idm.FederatedIdentityRepresentation;
+import org.keycloak.representations.idm.GroupRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.openpaas.servicebroker.container.platform.common.CommonStatusCode;
+import org.openpaas.servicebroker.container.platform.common.CommonUtils;
+import org.openpaas.servicebroker.container.platform.model.Constants;
+import org.openpaas.servicebroker.container.platform.model.KeycloakUserStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Keycloak Admin Client에 관한 서비스 클래스
+ * @author kjhoon
+ * @since 20210804
+ * @version 20210804
+ */
+@Service
+public class KeycloakAdminClientService {
+
+    @Value("${keycloak.admin.client.grantType}")
+    private String grantType;
+
+    @Value("${keycloak.admin.client.username}")
+    private String username;
+
+    @Value("${keycloak.admin.client.password}")
+    private String password;
+
+    @Value("${keycloak.admin.client.serverUrl}")
+    private String serverUrl;
+
+    @Value("${keycloak.admin.client.realm}")
+    private String realm;
+
+    @Value("${keycloak.admin.client.clientId}")
+    private String clientId;
+
+    @Value("${keycloak.admin.cp.realm}")
+    private String cpRealm;
+
+    @Value("${keycloak.admin.cp.IdentityProviderId}")
+    private String IdentityProviderId;
+
+    @Value("${keycloak.admin.cp.clusterAdminGroup}")
+    private String clusterAdminGroup;
+
+
+    private static final Logger logger = LoggerFactory.getLogger(KeycloakAdminClientService.class);
+
+    // build keycloak instance
+     public Keycloak getKeycloakInstance(){
+        return KeycloakBuilder.builder()
+                .grantType(grantType)
+                .username(username)
+                .password(password)
+                .serverUrl(serverUrl)
+                .realm(realm)
+                .clientId(clientId)
+                .build();
+    }
+
+
+    /**
+     * 사용자의 UAA Id를 전달받아 Keycloak 사용자 계정을 생성한다.
+     * FederatedIdentity를 통해 UAA 계정과 Keycloak 계정을 연계한다.
+     *
+     */
+    public KeycloakUserStatus createKeycloakUser(String username, String userType){
+
+        KeycloakUserStatus keycloakUserStatus = new KeycloakUserStatus();
+        Response response = null;
+
+        try {
+            UserRepresentation user = new UserRepresentation();
+            user.setUsername(username);
+            user.setEnabled(true);
+
+            // social login provider 정보 셋팅
+            FederatedIdentityRepresentation federatedIdentityRepresentation = new FederatedIdentityRepresentation();
+            federatedIdentityRepresentation.setIdentityProvider(IdentityProviderId);
+            federatedIdentityRepresentation.setUserId(username);
+            federatedIdentityRepresentation.setUserName(username);
+
+            List<FederatedIdentityRepresentation> federatedIdentityRepresentationsList= new ArrayList<>();
+            federatedIdentityRepresentationsList.add(federatedIdentityRepresentation);
+            user.setFederatedIdentities(federatedIdentityRepresentationsList);
+
+
+            // 클러스터 관리자의 경우 클러스터 관리자 그룹 셋팅
+            if(userType.equals(Constants.AUTH_CLUSTER_ADMIN)) {
+                List<String> groups = new ArrayList<>();
+                groups.add(clusterAdminGroup);
+                user.setGroups(groups);
+            }
+
+
+            // 사용자 생성
+            Keycloak keycloak = getKeycloakInstance();
+            response = keycloak.realm(cpRealm).users().create(user);
+            logger.info("Keycloak User Create Response Status : [{}] {}", CommonUtils.loggerReplace(response.getStatus()),
+                    CommonUtils.loggerReplace(response.getStatusInfo().getReasonPhrase()));
+        }
+        catch (Exception e) {
+            logger.info("### EXCEPTION OCCURRED DURING KEYCLOAK USER CREATE");
+            keycloakUserStatus.setResultCode(Constants.RESULT_STATUS_FAIL);
+
+            return keycloakUserStatus;
+        }
+
+
+        keycloakUserStatus.setResultCode(String.valueOf(response.getStatus()));
+
+        // 신규 생성된 계정인 경우 keycloak user id 셋팅
+        if(String.valueOf(response.getStatus()).equals(CommonStatusCode.CREATED.getCode())) {
+            keycloakUserStatus.setUserId(getKeycloakUserDetails(username).getId());
+        }
+
+        return keycloakUserStatus;
+    }
+
+
+
+    /**
+     * Keycloak 사용자 ID 를 전달받아 Keycloak 사용자 계정을 삭제한다.
+     *
+     */
+    public String deleteKeycloakUser(String userId) {
+
+        Response response = null;
+
+        try {
+            Keycloak keycloak = getKeycloakInstance();
+            response = keycloak.realm(cpRealm).users().delete(userId);
+            logger.info("Keycloak User Delete Response Status : [{}] {}", CommonUtils.loggerReplace(response.getStatus()),
+                    CommonUtils.loggerReplace(response.getStatusInfo().getReasonPhrase()));
+        }
+        catch (Exception e) {
+            logger.info("### EXCEPTION OCCURRED DURING KEYCLOAK USER DELETE");
+            return Constants.RESULT_STATUS_FAIL;
+        }
+
+        return String.valueOf(response.getStatus());
+    }
+
+
+    /**
+     * Keycloak 사용자 username을 통해 사용자 상세 정보를 조회한다.
+     *
+     */
+    public UserRepresentation getKeycloakUserDetails(String username) {
+
+        UserRepresentation userRepresentation = new UserRepresentation();
+
+        try {
+            Keycloak keycloak = getKeycloakInstance();
+            List<UserRepresentation> users = keycloak.realm(cpRealm).users().search(username, true);
+            userRepresentation = users.get(0);
+        }
+        catch(Exception e) {
+            logger.info("### EXCEPTION OCCURRED DURING GET KEYCLOAK USER INFO");
+        }
+
+        return userRepresentation;
+    }
+
+
+    /**
+     * Keycloak 내 사용자와 그룹의 맵핑 및 해제를 관리한다.
+     *
+     */
+    public String manageClusterAdminGroupToUser(String username, String type) {
+
+        try {
+            Keycloak keycloak = getKeycloakInstance();
+
+            // get user
+            UserRepresentation ur = getKeycloakUserDetails(username);
+
+            // find cluster admin group id
+            List<GroupRepresentation> groupRepresentationList = keycloak.realm(cpRealm).groups().groups();
+            List<GroupRepresentation> findCaGroup = groupRepresentationList.stream().filter(x -> x.getName().matches(clusterAdminGroup)).collect(Collectors.toList());
+            GroupRepresentation gr = findCaGroup.get(0);
+
+            if(type.equals(Constants.TYPE_JOIN)) {
+                logger.info("### JOIN THE CLUSTER ADMIN GROUP TO USER");
+                keycloak.realm(cpRealm).users().get(ur.getId()).joinGroup(gr.getId());
+            }
+            else {
+                logger.info("### LEAVE THE CLUSTER ADMIN GROUP TO USER");
+                keycloak.realm(cpRealm).users().get(ur.getId()).leaveGroup(gr.getId());
+            }
+
+        }
+        catch(Exception e) {
+            logger.info("### EXCEPTION OCCURRED DURING ADD CLUSTER ADMIN GROUP TO USER");
+            return Constants.RESULT_STATUS_FAIL;
+        }
+
+        return Constants.RESULT_STATUS_SUCCESS;
+    }
+
+
+
+
+}
